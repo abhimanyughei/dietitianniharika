f1c9a7f0512aeb6bc4cd27c69377bf7fd50fc98d
Abhimanyu Ghei <abhimanyughei@gmail.com>
2026-05-24 14:58:58 -0400

    Feat: package upgrades

--- tina/config.js
+++ tina/config.js
@@ -15,7 +15,7 @@ import {
 } from "./templates";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch = process.env.GITHUB_REF_NAME || process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
 
 export default defineConfig({
   branch,
